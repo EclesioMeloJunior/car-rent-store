a953ca779aeb7a6bbf0776d70defc0fe3c965e34
Eclesio Ferreira <eclesio@alphasoftware.br>
2019-08-16 14:33:49 -0400

    feat: rotas aplicadas na navbar

--- admin/src/components/Navbar/index.js
+++ admin/src/components/Navbar/index.js
@@ -1,21 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { default as BootstrapNavbar } from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 
 const Navbar = () => (
 	<BootstrapNavbar bg="dark" variant="dark">
-		<BootstrapNavbar.Brand href="#home">BootstrapNavbar</BootstrapNavbar.Brand>
+		<BootstrapNavbar.Brand href="#home">Car Rent Store</BootstrapNavbar.Brand>
 		<Nav className="mr-auto">
-			<Nav.Link href="#home">Home</Nav.Link>
-			<Nav.Link href="#features">Features</Nav.Link>
-			<Nav.Link href="#pricing">Pricing</Nav.Link>
+			<Link className="nav-link" to="/carros">
+				Meus Carros
+			</Link>
+			<Link className="nav-link" to="/cidades">
+				Cidades
+			</Link>
+			<Link className="nav-link" to="/configuracoes">
+				Configurações
+			</Link>
 		</Nav>
-		<Form inline>
-			<Form.Control type="text" placeholder="Search" className="mr-sm-2" />
-			<Button variant="outline-info">Search</Button>
-		</Form>
 	</BootstrapNavbar>
 );
 
