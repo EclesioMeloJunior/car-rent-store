33cd37036991150141010ea7a6303efd4e6b669d
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2019-11-28 14:30:33 -0400

    algumas mudanças

--- admin/src/firebase/index.js
+++ admin/src/firebase/index.js
@@ -4,19 +4,19 @@ import "firebase/firestore";
 import "firebase/functions";
 
 var firebaseConfig = {
-	apiKey: "AIzaSyA4Vys1jo8XdpDYnnPBhfnNTn8dgHTk99g",
-	authDomain: "car-rent-store.firebaseapp.com",
-	databaseURL: "https://car-rent-store.firebaseio.com",
-	projectId: "car-rent-store",
-	storageBucket: "car-rent-store.appspot.com",
-	messagingSenderId: "273341965801",
-	appId: "1:273341965801:web:39342d81a1281b58"
+  apiKey: "AIzaSyA4Vys1jo8XdpDYnnPBhfnNTn8dgHTk99g",
+  authDomain: "car-rent-store.firebaseapp.com",
+  databaseURL: "https://car-rent-store.firebaseio.com",
+  projectId: "car-rent-store",
+  storageBucket: "car-rent-store.appspot.com",
+  messagingSenderId: "273341965801",
+  appId: "1:273341965801:web:39342d81a1281b58"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-if (process.env.NODE_ENV === "development") {
-	firebase.functions().useFunctionsEmulator("http://localhost:5000");
-}
+// if (process.env.NODE_ENV === "development") {
+// 	firebase.functions().useFunctionsEmulator("http://localhost:5000");
+// }
 
 export default firebase;
